34b19e7439139fecaeaa05f06dfd70d1292aa1dc
Wembyyy <Cwbfsimiyu@gmail.com>
2024-02-22 10:11:30 +0300

    lesson on loops

---
+++ week_1/strings.py
@@ -0,0 +1,39 @@
+# name: chris
+city = "nairobi"
+# convert the upper case
+print(city)
+print("\n") # print a new line
+print(city.upper()) # converts string to upper case
+name = "CHRIS"
+print(name.lower())# converts string to lower case
+town ="   Naivasha    "
+
+print(town)
+print("\t") # will print new term
+print(town.strip())
+
+f_name ="Chris"
+s_name ="wekesa"
+full_name=f_name+s_name
+print(full_name)
+
+print(city[0])
+print(city[1])
+print(city[2])
+print(city[3])
+print(city[4])
+print(city[5])
+print(city[6])
+print(city[-1])
+print(city[-2])
+# write a program to reverse a string
+# write a program to reverse a palindrome
+#replacing a character
+fruit=  "orange"
+print(fruit.replace('o','y'))
+subject= "Physical:Sciences"
+print(subject.split(":"))
+
+age=18
+height=1.2
+print("I am {0}years old and {1} meters tall" .format(age,height))
---
+++ week_2/loops.py
@@ -0,0 +1,13 @@
+# name: Chris
+# date: 2/22/2024
+
+
+#for loop
+for number in range(0,10):
+    print(number)
+
+#squares of the number
+for x in range(0,11):
+    print(str(x**2) + "\t",end=" ") # gives you the squares
+#print the multiplication table
+    
