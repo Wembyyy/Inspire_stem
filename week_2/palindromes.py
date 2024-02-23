d2da3c1bd08f96b26397a265d9fd22d8c08d1af6
Wembyyy <Cwbfsimiyu@gmail.com>
2024-02-23 08:21:17 +0300

    deez

---
+++ week_2/palindromes.py
@@ -0,0 +1,12 @@
+#program that reveals if a word is a palindrome
+#name:chris
+#date:2/23/2024
+
+character=str(input("enter the desired word"))
+new_character=character
+print(new_character)
+
+if(new_character:= str(character)):
+    print("the world is a palindrome")
+else:
+    print("the world is not a palindrome")
